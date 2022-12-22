934bc00d4457587b200dffb434f377ec69581128
Zaidkhan888 <zaid.201994101@vcet.edu.in8>
2022-12-22 10:22:21 +0530

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# VCET SDP Stack Project 
+> This project  follwo MVC and  RestFull design pattern
+
+## MVC
+1. Model - database acess and schema
+2. View - to show data
+3. COntroller - to manipulate data
---
+++ first.js
@@ -0,0 +1,11 @@
+/*
+1. MVC + REST 
+2. Lint == grammer for coding --> ESLint
+3. Api == view engine ===> controlling view 
+    /login   /register(Auth)  /verifyEmail(trigerring otp) 
+    Todo(not today0 ) , profile -- /profile 
+
+4. developre testing  Unit , integration , E2E (end to end)
+*/
+
+console.log("Hello world ");
